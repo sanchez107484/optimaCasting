7bf7281bb75c76aa8b005110ac662e5e2554c510
luis fernando sanchez merino <luissnxz@gmail.com>
2023-05-23 12:56:42 +0200

    navbar a la derecha

--- resources/views/layouts/navbars/guest/navbar.blade.php
+++ resources/views/layouts/navbars/guest/navbar.blade.php
@@ -18,25 +18,12 @@
                         </span>
                     </button>
                     <div class="collapse navbar-collapse" id="navigation">
-                        <ul class="navbar-nav mx-auto">
+                        <ul class="navbar-nav ms-auto">
                             <li class="nav-item">
                                 <a class="nav-link d-flex align-items-center me-2 active" aria-current="page"
                                     href="{{ route('about') }}">
                                     <i class="fa fa-chart-pie opacity-6 text-dark me-1"></i>
                                     @lang('navbar.about')
-                                </a>
-                            </li>
-                            {{-- <li class="nav-item">
-                                <a class="nav-link d-flex align-items-center me-2 active" aria-current="page"
-                                    href="{{ route('home') }}">
-                                    <i class="fa fa-chart-pie opacity-6 text-dark me-1"></i>
-                                    Dashboard
-                                </a>
-                            </li> --}}
-                            <li class="nav-item">
-                                <a class="nav-link me-2" href="{{ route('register') }}">
-                                    <i class="fas fa-user-circle opacity-6 text-dark me-1"></i>
-                                    @lang('navbar.register')
                                 </a>
                             </li>
                             <li class="nav-item">
